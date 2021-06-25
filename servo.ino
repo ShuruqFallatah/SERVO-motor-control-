73b9fd4f7a90baa0465983443b320e81dcf4897d
shuruq fallatah <shuruqfallatah@gmail.com>
2021-06-25 13:05:40 +0300

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -1,45 +1,84 @@
 #include <Servo.h>
 
-Servo base; 
+Servo base;
 Servo shoulder;
 Servo elbow;
-int a=0;
-int b=1;
+Servo hand;
+Servo grapper;
+int a;
+int pot1=1;
+int pot2=2;
+int pot3=3;
+int pot4=4;
+int pot5=5;
+
 void setup() {
-base. attach(3);
-shoulder. attach(4);
-elbow. attach (5);
+base. attach(2);
+shoulder. attach(3);
+elbow. attach (4);
+hand. attach (5);
+grapper. attach (6);
  
 }
 void loop(){
- for ( a =0; a<=180;a++){
+  a= analogRead(pot1);
+ for ( a;0, a<=90;a++){
 base.write(a);
 delay(15);
 }
 delay(1000);
- for ( a =0; a<=90;a++){
+a= analogRead(pot2);
+ for ( a;0, a<=90;a++){
 shoulder.write(a);
 delay(15);
 }
 delay(1000);
- for ( a =0; a<=360;a++){
+ a= analogRead(pot3);
+ for ( a;0, a<=90;a++){
 elbow.write(a);
 delay(15);
 }
 delay(1000);
- for ( a =180; a>=0;a){
+ a= analogRead(pot4);
+ for ( a;0, a<=90;a++){
+hand.write(a);
+delay(15);
+}
+delay(1000);
+ a= analogRead(pot5);
+ for ( a;0, a<=90;a++){
+grapper.write(a);
+delay(15);
+}
+delay(1000);
+ a= analogRead(pot1);
+ for ( a;90, a>=0;a--){
 base. write(a);
 delay(15);
 }
 delay(1000);
- for( a=90; a>=0;a){
+ a= analogRead(pot2);
+ for( a;90, a>=0;a--){
 shoulder.write(a);
 delay(15);
 }
 delay(1000);
- for( a =360; a>=0;a){
+  a= analogRead(pot3);
+ for( a; 90, a>=0;a--){
 elbow.write(a);
 delay(15);
 }
+delay(1000);
+  a= analogRead(pot4);
+ for( a; 90, a>=0;a--){
+hand.write(a);
+delay(15);
+ }
+delay(1000);
+  a= analogRead(pot5);
+ for( a;90, a>=0;a--){
+grapper.write(a);
+delay(15);
+ }
 delay(1000);
 }
